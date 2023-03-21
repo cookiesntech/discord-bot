560b585d0bd012265354173882e67a9db0834556
Marina <97986457+cookiesntech@users.noreply.github.com>
2023-03-21 11:31:13 +0100

    ISSUE 1: Initial discord and dotenv setup

---
+++ bot.py
@@ -0,0 +1,27 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+client.run(TOKEN)
